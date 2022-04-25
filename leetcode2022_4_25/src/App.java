837087b8fa60fb602bcce97fe9c0e6f1a4c02d2d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-04-25 15:07:08 +0800

    Add files via upload

---
+++ leetcode2022_4_25/src/App.java
@@ -0,0 +1,11 @@
+import leetcode.Solution;
+
+public class App {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution(new int[]{1, 3, 5, 3, 4, 2});
+        System.out.println(sol.pick(3));
+
+    }
+
+}
