ce1238a5c9e119ce179ddb0b09c083e2cb1fa201
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-05-26 14:06:43 +0800

    Update Solution.java

--- leetcode2022_5_26/src/leetcode/Solution.java
+++ leetcode2022_5_26/src/leetcode/Solution.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import com.sun.source.tree.Tree;
+
+import java.util.Objects;
 import java.util.Stack;
 
 /**
@@ -16,18 +19,65 @@ import java.util.Stack;
  */
 public class Solution {
 
-    public Stack<Integer> stack1;
-    public Stack<Integer> stack2;
+//    public Stack<Integer> stack1;
+//    public Stack<Integer> stack2;
 
-    public void preOrder(TreeNode root, TreeNode t) {
-        if (root == null || root == t) {
-            return;
+    public boolean getPath(TreeNode root, TreeNode node, Stack<TreeNode> stack) {
+        if (root == null || node == null) {
+            return false;
         }
-        preOrder(root.left, t);
-        preOrder(root.right, t);
+        stack.push(root);
+        if (root == node) {
+            return true;
+        }
+        boolean key = getPath(root.left, node, stack);
+        if (key) {
+            return true;
+        }
+        key = getPath(root.right, node, stack);
+        if (key) {
+            return true;
+        }
+        stack.pop();
+        return false;
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        preOrder(root,p);
+        Stack<TreeNode> stack1 = new Stack<>();
+        getPath(root, p, stack1);
+        Stack<TreeNode> stack2 = new Stack<>();
+        getPath(root, q, stack2);
+        int diff = stack1.size() - stack2.size();
+        while (diff > 0) {
+            diff--;
+            stack1.pop();
+        }
+        while (diff < 0) {
+            diff++;
+            stack2.pop();
+        }
+        while (stack1.size() > 0) {
+            TreeNode cur = stack1.pop();
+            if (Objects.equals(cur, stack2.pop())) {
+                return cur;
+            }
+        }
+        return null;
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor2(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor2(root.right, p, q);
+        if (leftNode != null && rightNode != null) {
+            return root;
+        } else if (leftNode != null) {
+            return leftNode;
+        } else return rightNode;
     }
 }
