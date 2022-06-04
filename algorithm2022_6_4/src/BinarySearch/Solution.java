8bdc66db21cf5df02b8a0d2c169b0e78ad4855ce
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-04 14:06:19 +0800

    add guessNumber method

---
+++ algorithm2022_6_4/src/BinarySearch/Solution.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : algorithm2022_6_4
+ * @Package : BinarySearch
+ * @createTime : 2022/6/4 13:56
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Solution {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int guessNumber(int n) {
+        int left = 0;
+        int right = n;
+        while(left<=right){
+            int mid = left+((right-left)>>>1);
+            // one method given by leetcode
+            if(guess(mid)==-1){
+                right=mid-1;
+            }else if(guess(mid)==0){
+                return mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        return 0;
+    }
+
+}
