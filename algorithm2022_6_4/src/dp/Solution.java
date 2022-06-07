3c5aa37689e2f629f8c15112d6ea1f4f628a2822
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-07 09:41:39 +0800

    add dp method==>canJump

--- algorithm2022_6_4/src/dp/Solution.java
+++ algorithm2022_6_4/src/dp/Solution.java
@@ -14,6 +14,20 @@ package dp;
  */
 public class Solution {
 
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        int most = 0;
+        for (int i = 0; i < len; i++) {
+            if (i <= most) {
+                most = Math.max(most, i + nums[i]);
+                if (most >= len - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public int maxProfit(int[] prices) {
         int min = prices[0];
         int max = 0;
