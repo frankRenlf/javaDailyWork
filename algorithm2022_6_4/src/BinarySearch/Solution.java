edcd0aa4f00b0c7b790eab30cb9ec32bc907ae32
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-09 11:13:53 +0800

    add BinarySearchmethod==>specialArray

--- algorithm2022_6_4/src/BinarySearch/Solution.java
+++ algorithm2022_6_4/src/BinarySearch/Solution.java
@@ -14,6 +14,39 @@ package BinarySearch;
  */
 public class Solution {
 
+    public int specialArray(int[] nums) {
+        int n = nums.length;
+        int right = 0;
+        for (int num : nums) {
+            right = Math.max(right, num);
+        }
+
+        int left = 1;
+        right = Math.min(right, n);
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int bigger = check(nums, mid);
+            if (bigger == mid)
+                return mid;
+            if (bigger > mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private int check(int[] nums, int mid) {
+        int n = nums.length;
+        int cnt = 0;
+        for (int num : nums) {
+            if (num >= mid) ++cnt;
+        }
+        return cnt;
+    }
+
     public int[] twoSum2(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
         while (left < right) {
