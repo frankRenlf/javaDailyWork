80fcbb1a9bcdb2b4c92db507d1a390baaf917f6f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-12 10:47:23 +0800

    add new dp method==>trap

--- algorithm2022_6_4/src/dp/Solution.java
+++ algorithm2022_6_4/src/dp/Solution.java
@@ -18,6 +18,28 @@ import java.util.Set;
  */
 public class Solution {
 
+    public int trap(int[] height) {
+        int len = height.length;
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < len; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        rightMax[len - 1] = height[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < len; i++) {
+            ret += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ret;
+    }
+
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
