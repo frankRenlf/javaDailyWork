14f7d3cd2531067e15643a841c485ca25d756348
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-12 11:03:36 +0800

    add BinarySearch method==>searchInsert2

--- algorithm2022_6_4/src/BinarySearch/Solution.java
+++ algorithm2022_6_4/src/BinarySearch/Solution.java
@@ -14,6 +14,20 @@ package BinarySearch;
  */
 public class Solution {
 
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public int search3(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
