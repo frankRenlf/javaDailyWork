1a4daffe4f2d3d6fa383b5b9de9df332392c910c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-16 16:01:43 +0800

    add rec2 method

---
+++ 2022_6_13_test1/src/success/settle_stack.java
@@ -0,0 +1,48 @@
+package success;
+
+import test1.Vehicles;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : 2022_6_13_test1
+ * @Package : success
+ * @createTime : 2022/6/16 15:37
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class settle_stack {
+    static Vehicles vehicles = new Vehicles();
+
+    public static double single(double tw) {
+        if (tw <= 0) {
+            return 0;
+        }
+        List<Double> tmp = new ArrayList<>();
+        for (int i = 0; i < vehicles.getVehicleList().size(); i++) {
+            tmp.add(vehicles.getVehicleList().get(i).getPrice()
+                    + single(tw - vehicles.getVehicleList().get(i).getWeight()));
+        }
+        tmp.sort(new Comparator<Double>() {
+            @Override
+            public int compare(Double o1, Double o2) {
+                return Double.compare(o1, o2);
+            }
+        });
+        return tmp.get(0);
+    }
+
+    public static void main(String[] args) {
+        Stack<Double> stack = new Stack<>();
+
+    }
+
+}
