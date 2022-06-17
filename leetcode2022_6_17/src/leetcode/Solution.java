96189506147eced6bcbbec52ceb7a51fbe7966cc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-17 14:03:46 +0800

    add insertIntoBST method

--- leetcode2022_6_17/src/leetcode/Solution.java
+++ leetcode2022_6_17/src/leetcode/Solution.java
@@ -14,6 +14,31 @@ package leetcode;
  */
 public class Solution {
 
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode ret = root;
+        while (root != null) {
+            if (root.val > val) {
+                if (root.left != null) {
+                    root = root.left;
+                } else {
+                    root.left = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if (root.right != null) {
+                    root = root.right;
+                } else {
+                    root.right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        if (ret == null) {
+            ret = new TreeNode(val);
+        }
+        return ret;
+    }
+
     public TreeNode searchBST(TreeNode root, int val) {
         while (root != null) {
             if (root.val == val) {
