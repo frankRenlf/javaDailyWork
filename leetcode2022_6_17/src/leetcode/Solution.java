1ee7b9daa3888e2a437a4cafca9886585ec3e357
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-18 14:38:07 +0800

    add dp method==>minPathSum

--- leetcode2022_6_17/src/leetcode/Solution.java
+++ leetcode2022_6_17/src/leetcode/Solution.java
@@ -19,6 +19,43 @@ import java.util.Set;
  */
 public class Solution {
 
+    public int minPathSum2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
         if (obstacleGrid == null || obstacleGrid.length == 0) {
             return 0;
