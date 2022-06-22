444ce47ab9da05e53585aaeb56221bdf094dc02f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-22 09:55:01 +0800

    initiate structure

---
+++ leetcode2022_6_22/src/leetcode/Solution.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : leetcode2022_6_22
+ * @Package : leetcode
+ * @createTime : 2022/6/22 9:54
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Solution {
+}
