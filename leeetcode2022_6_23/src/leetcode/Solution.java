157d6f7ab6d454fbe6acacce755b41dbf7b72ab4
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-23 10:34:16 +0800

    leetcode dp method ==>change

---
+++ leeetcode2022_6_23/src/leetcode/Solution.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : leeetcode2022_6_23
+ * @Package : leetcode
+ * @createTime : 2022/6/23 10:08
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int len = coins.length;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < len; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
---
+++ leeetcode2022_6_23/src/test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : leeetcode2022_6_23
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/6/23 10:08
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class test {
+}
