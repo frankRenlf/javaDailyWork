eb38c373e6ee19f753dbb5e09f943f834e65ff4a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-26 11:11:11 +0800

    modify mincost method to reduce the space complexity

--- leetcode2022_6_26/src/leetcode/Solution.java
+++ leetcode2022_6_26/src/leetcode/Solution.java
@@ -17,16 +17,18 @@ public class Solution {
     public int minCost2(int[][] costs) {
         int num = costs.length;
         int len = costs[0].length;
-        int[][] dp = new int[num][len];
-        System.arraycopy(costs[0], 0, dp[0], 0, len);
+        int[] dp = new int[len];
+        System.arraycopy(costs[0], 0, dp, 0, len);
         for (int i = 1; i < num; i++) {
+            int[] tmp = new int[len];
             for (int j = 0; j < len; j++) {
-                dp[i][j] = Math.min(dp[i - 1][(j + 1) % len], dp[i - 1][(j + 2) % 3]) + costs[i][j];
+                tmp[j] = Math.min(dp[(j + 1) % len], dp[(j + 2) % 3]) + costs[i][j];
             }
+            dp = tmp;
         }
-        int ret = dp[num - 1][0];
-        for (int i = 1; i < len; i++) {
-            ret = Math.min(ret, dp[num - 1][i]);
+        int ret = dp[0];
+        for (int j = 1; j < len; j++) {
+            ret = Math.min(ret, dp[j]);
         }
         return ret;
     }
