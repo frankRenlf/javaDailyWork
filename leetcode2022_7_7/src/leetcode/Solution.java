ec73fdeb40f9b0926954a8a9b0c6279bf14e8ddc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-08 11:24:11 +0800

    add daily leetcode method==>minCostToMoveChips

--- leetcode2022_7_7/src/leetcode/Solution.java
+++ leetcode2022_7_7/src/leetcode/Solution.java
@@ -60,4 +60,17 @@ public class Solution {
         }
     }
 
+
+    public int minCostToMoveChips(int[] position) {
+        int even = 0, odd = 0;
+        for (int pos : position) {
+            if ((pos & 1) != 0) {
+                odd++;
+            } else {
+                even++;
+            }
+        }
+        return Math.min(odd, even);
+    }
+
 }
