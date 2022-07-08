703cc2d04a4815ea9fbe96c59ac338abd5071147
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-08 16:41:43 +0800

    add maxSubArray

--- leetcode2022_7_7/src/leetcode/Solution.java
+++ leetcode2022_7_7/src/leetcode/Solution.java
@@ -73,4 +73,15 @@ public class Solution {
         return Math.min(odd, even);
     }
 
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int pre = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < len; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            max = Math.max(max, pre);
+        }
+        return max;
+    }
+
 }
