a571ae63bebbb7f4159c41575af5db6db3af91b4
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-20 16:06:12 +0800

    Update Solution.java

--- leetcode2022_7_20/src/leetcode/Solution.java
+++ leetcode2022_7_20/src/leetcode/Solution.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +18,37 @@ import java.util.List;
 public class Solution {
 
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int step = k % (row * col);
+        int[][] tmp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                tmp[(i + step / col) % row][(j + step % col) % col] = grid[i][j];
+            }
+        }
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < row; i++) {
+            List<Integer> elem = new ArrayList<>();
+            for (int j = 0; j < col; j++) {
+                elem.add(tmp[i][j]);
+            }
+            ret.add(elem);
+        }
+        return ret;
+    }
 
+    public List<List<Integer>> shiftGrid1(int[][] grid, int k) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int step = k % (row * col);
+        int[][] ret = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                ret[i + k / col][j + k % col] = grid[i][j];
+            }
+        }
+        return
     }
 
 }
