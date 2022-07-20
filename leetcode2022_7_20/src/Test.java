57a61a76416371dfddda8a891dc335802ba42c86
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-20 15:42:58 +0800

    initiate new project

---
+++ leetcode2022_7_20/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : leetcode2022_7_20
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/7/20 15:42
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Test {
+}
---
+++ leetcode2022_7_20/src/leetcode/Solution.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : leetcode2022_7_20
+ * @Package : leetcode
+ * @createTime : 2022/7/20 15:42
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Solution {
+}
