9e846713afb97addceebc9e8d28740d27b6f3fb3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-22 12:23:04 +0800

    Update Solution.java

--- leetcode2022_7_20/src/leetcode/Solution.java
+++ leetcode2022_7_20/src/leetcode/Solution.java
@@ -183,4 +183,14 @@ public class Solution {
         return slow;
     }
 
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != val) {
+                nums[left++] = nums[right];
+            }
+        }
+        return left;
+    }
+
 }
