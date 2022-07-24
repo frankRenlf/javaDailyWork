56e48bd91a377e7f92f8ac3f32c73093e7d7b794
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-24 14:40:07 +0800

    Create Solution.java

---
+++ SummerWork/src/july24/Solution.java
@@ -0,0 +1,21 @@
+package july24;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : SummerWork
+ * @Package : july24
+ * @createTime : 2022/7/24 14:39
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class Solution {
+
+    public int addDigits(int num) {
+
+    }
+
+}
