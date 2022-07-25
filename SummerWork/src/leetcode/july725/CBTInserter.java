b942af2f6015640ab68a7862de87c165de578947
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-25 14:42:08 +0800

    Update CBTInserter.java

--- SummerWork/src/leetcode/july725/CBTInserter.java
+++ SummerWork/src/leetcode/july725/CBTInserter.java
@@ -22,11 +22,14 @@ public class CBTInserter {
     TreeNode root;
     Queue<TreeNode> queue;
 
+    // 这个leetcode的时间复杂度还是有点问题的,一会块一会,慢. 估计和网速有关,或者他们运行代码的服务器拥挤问题.
+    // 在这道题中,我用queue先将未填满的父节点都储存下来,这样虽然在initiate时需要耗费O(n),但是在后面多次插入时,只需要O(1),非常方便.
     public CBTInserter(TreeNode root) {
         this.root = root;
         queue = new LinkedList<>();
         Queue<TreeNode> tmp = new LinkedList<>();
         tmp.add(root);
+        // 使用广度优先遍历,将最后两行的未填满的节点都存入queue中.
         while (!tmp.isEmpty()) {
             TreeNode cur = tmp.poll();
             if (cur.left != null) {
@@ -41,6 +44,8 @@ public class CBTInserter {
         }
     }
 
+    //这样在insert时可以迅速插入,复杂度达到O(1).
+    // 整体需要花费O(n)的额外空间
     public int insert(int val) {
         TreeNode child = new TreeNode(val);
         TreeNode parent = queue.peek();
