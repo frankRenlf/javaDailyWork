9a1b30564142951736d52940ba7f10e66a041798
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-28 11:41:19 +0800

    update july28 solution

---
+++ SummerWork/src/structure/ListNode.java
@@ -0,0 +1,22 @@
+package structure;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : SummerWork
+ * @Package : structure
+ * @createTime : 2022/7/28 11:22
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
