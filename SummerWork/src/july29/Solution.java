aedd05b0d86d991e06db2b82e5ead0af25e5c8cb
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-29 11:10:33 +0800

    Create Solution.java

---
+++ SummerWork/src/july29/Solution.java
@@ -0,0 +1,62 @@
+package july29;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : SummerWork
+ * @Package : july29
+ * @createTime : 2022/7/29 11:05
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Solution {
+
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        if (Arrays.equals(p1, p2)) {
+            return false;
+        }
+        if (help(p1, p2, p3, p4)) {
+            return true;
+        }
+        if (Arrays.equals(p1, p3)) {
+            return false;
+        }
+        if (help(p1, p3, p2, p4)) {
+            return true;
+        }
+        if (Arrays.equals(p1, p4)) {
+            return false;
+        }
+        if (help(p1, p4, p2, p3)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean help(int[] p1, int[] p2, int[] p3, int[] p4) {
+        int[] v1 = {p1[0] - p2[0], p1[1] - p2[1]};
+        int[] v2 = {p3[0] - p4[0], p3[1] - p4[1]};
+        return checkMidPoint(p1, p2, p3, p4) && checkLength(v1, v2) && calCos(v1, v2);
+    }
+
+    public boolean checkLength(int[] v1, int[] v2) {
+        return (v1[0] * v1[0] + v1[1] * v1[1]) == (v2[0] * v2[0] + v2[1] * v2[1]);
+    }
+
+    public boolean checkMidPoint(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return (p1[0] + p2[0]) == (p3[0] + p4[0]) && (p1[1] + p2[1]) == (p3[1] + p4[1]);
+    }
+
+    public boolean calCos(int[] v1, int[] v2) {
+        return (v1[0] * v2[0] + v1[1] * v2[1]) == 0;
+    }
+
+}
+
+
+
