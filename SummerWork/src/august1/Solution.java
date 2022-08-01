04f29f06566eb0ad4f63fafb8ec738466453ff57
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-01 16:44:10 +0800

    Create Solution.java

---
+++ SummerWork/src/august1/Solution.java
@@ -0,0 +1,59 @@
+package august1;
+
+import structure.ListNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : SummerWork
+ * @Package : august1
+ * @createTime : 2022/8/1 14:01
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Solution {
+
+    public String generateTheString(int n) {
+        StringBuffer stringBuffer = new StringBuffer();
+        if (n % 2 == 0) {
+            return stringBuffer.append("a".repeat(n - 1)).append("b").toString();
+        }
+        return stringBuffer.append("a".repeat(n)).toString();
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+
+}
