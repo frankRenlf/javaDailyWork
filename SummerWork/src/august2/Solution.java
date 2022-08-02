9863522481a06b32c41fc2175c927f41d10207fc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-02 16:43:24 +0800

    Update Solution.java

--- SummerWork/src/august2/Solution.java
+++ SummerWork/src/august2/Solution.java
@@ -1,7 +1,6 @@
 package august2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -31,5 +30,23 @@ public class Solution {
         }
         return list;
     }
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int val = nums2[i];
+            while (!stack.isEmpty() && val >= stack.peek()) {
+                stack.pop();
+            }
+            map.put(val, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(val);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.get(nums1[i]);
+        }
+        return nums1;
+    }
+
 
 }
